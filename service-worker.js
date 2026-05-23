e76cd6af658ff8c0a52a95d5455dc771059309a0
whentrichjr <whentrichjr@gmail.com>
2026-05-23 12:28:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  *   - Everything else: network-first
  */
 
-const CACHE_NAME   = 'tol-v1';
+const CACHE_NAME   = 'tol-v2';
 const GAS_ORIGIN   = 'script.google.com';
 const FONT_ORIGIN  = 'fonts.googleapis.com';
 const GSTATIC      = 'fonts.gstatic.com';
