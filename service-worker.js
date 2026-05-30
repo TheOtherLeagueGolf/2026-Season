acb8b3b4580a27971822124466ea099cb6860085
whentrichjr <whentrichjr@gmail.com>
2026-05-30 07:59:13 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
  *   - Everything else: network-first
  */
 
-const CACHE_NAME   = 'tol-v15';
+const CACHE_NAME   = 'tol-v16';
 const GAS_ORIGIN   = 'script.google.com';
 const FONT_ORIGIN  = 'fonts.googleapis.com';
 const GSTATIC      = 'fonts.gstatic.com';
